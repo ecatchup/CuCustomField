d3a870f29a384a377ff0766f5a5484f4485cd594
komori <komori@catchup.co.jp>
2025-12-15 17:07:48 +0900

    DBテーブルのスキーマファイルを調整

--- Config/Schema/cu_custom_field_values.php
+++ Config/Schema/cu_custom_field_values.php
@@ -13,15 +13,14 @@ class CuCustomFieldValuesSchema extends CakeSchema {
 	public $cu_custom_field_values = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary', 'comment' => 'ID'),
 		'relate_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'comment' => '関連コンテンツID'),
-		'key' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '保存キー', 'charset' => 'utf8'),
-		'value' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '保存値', 'charset' => 'utf8'),
-		'model' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '保存モデル名', 'charset' => 'utf8'),
+		'key' => array('type' => 'string', 'null' => true, 'default' => null, 'comment' => '保存キー'),
+		'value' => array('type' => 'text', 'null' => true, 'default' => null, 'comment' => '保存値'),
+		'model' => array('type' => 'string', 'null' => true, 'default' => null, 'comment' => '保存モデル名'),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '更新日時'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '作成日時'),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1)
 		),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
 
 }
