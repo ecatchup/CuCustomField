65564d9c5a74c4d864345eb9ee049371ed393870
kato <kato@e-catchup.jp>
2026-09-02 20:50:42 +0900

    adminLinkの記述方法が正しくなかったので修正

--- config.php
+++ config.php
@@ -14,6 +14,6 @@ return [
 	'description' => 'ブログ記事に入力欄を設定できます。',
 	'author' => '	Catchup, inc.',
 	'url' => 'https://catchup.co.jp/',
-	'adminLink' => ['plugin' => 'cu_custom_field', 'controller' => 'cu_custom_field_configs', 'action' => 'index'],
+	'adminLink' => ['plugin' => 'CuCustomField', 'controller' => 'cu_custom_field_configs', 'action' => 'index'],
 	'installMessage' => 'ブログ記事の投稿画面に入力欄が追加されます。<br>インストール直後は、各ブログコンテンツのカスタムフィールドは無効状態です。カスタムフィールド設定画面より、利用するブログを有効に変更したのち利用してください。<br>また、１つのフィールドに大量の文字列を追加する可能性がある場合は、<br />データベース内の以下のテーブル内のカラム「value」の型を「LONGTEXT」に変更してください。<ul><li>cu_custom_field_values</li></ul>',
 ];
